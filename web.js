3fcc519774b69f7d4c9f586574e4a544a3786bb2
Ubuntu <ubuntu@ip-172-31-47-5.us-west-2.compute.internal>
2013-07-20 03:03:18 +0000

    Changed web.js try statics

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static(__dirname));
 var fs = require('fs');
 
 app.get('/', function(request, response) {
